b33fa7b62b2713f845c9e2efc7c974487da43dcf
heweiwei <2125188145@qq.com>
2020-03-22 18:34:20 +0800

    test-合并

--- cloud-consumer-zookeeper-order80/src/main/java/com/hww/controller/OrderController.java
+++ cloud-consumer-zookeeper-order80/src/main/java/com/hww/controller/OrderController.java
@@ -23,7 +23,6 @@ public class OrderController {
 
     @GetMapping("/consumer/payment/zk")
     private String paymentInfo() {
-        log.info("develop");
         return restTemplate.getForObject(zk_service_url + "/payment/zk", String.class);
     }
 }
